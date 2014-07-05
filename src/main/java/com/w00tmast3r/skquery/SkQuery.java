cc4856980c74f7eedc17af50fd868f65511507c6
Roll Fizzlebeef <bryan.terce@gmail.com>
2014-07-05 15:45:51 -0700

    Add Early API Hooks to imitate SkAddonLib

--- src/main/java/com/w00tmast3r/skquery/SkQuery.java
+++ src/main/java/com/w00tmast3r/skquery/SkQuery.java
@@ -1,6 +1,28 @@
 package com.w00tmast3r.skquery;
 
+import ch.njol.skript.Skript;
+import ch.njol.skript.SkriptAddon;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SkQuery extends JavaPlugin {
+public final class SkQuery extends JavaPlugin {
+
+    private static SkQuery instance = null;
+    private static SkriptAddon addonInstance = null;
+
+    public SkQuery() {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+        addonInstance = Skript.registerAddon(this);
+    }
+
+    public static SkQuery getInstance() {
+        return instance;
+    }
+
+    public static SkriptAddon getAddonInstance() {
+        return addonInstance;
+    }
 }
