bd16ba30d58711d6c5a2750aaf74efb1e69458d1
Roll Fizzlebeef <bryan.terce@gmail.com>
2014-08-10 09:47:15 -0700

    Add a condition for boolean statements

---
+++ src/main/java/com/w00tmast3r/skquery/elements/conditions/CondBoolean.java
@@ -0,0 +1,39 @@
+package com.w00tmast3r.skquery.elements.conditions;
+
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Checker;
+import ch.njol.util.Kleenean;
+import com.w00tmast3r.skquery.api.Patterns;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+
+@Patterns({"%boolean%", "not %boolean%"})
+public class CondBoolean extends Condition {
+
+    private Expression<Boolean> bool;
+    boolean negated;
+
+    @Override
+    public boolean check(Event e) {
+        return bool.check(e, new Checker<Boolean>() {
+            @Override
+            public boolean check(Boolean o) {
+                return o;
+            }
+        }, negated);
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "bool condition";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        bool = (Expression<Boolean>) exprs[0];
+        negated = matchedPattern == 1;
+        return true;
+    }
+}
