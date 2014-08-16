02d0ee85d0cdb622ca02c951e7502c489980124a
Roll Fizzlebeef <bryan.terce@gmail.com>
2014-08-15 21:35:55 -0700

    Fix NPE when using SkQuery version

--- src/main/java/com/w00tmast3r/skquery/elements/expressions/ExprSkQueryVersion.java
+++ src/main/java/com/w00tmast3r/skquery/elements/expressions/ExprSkQueryVersion.java
@@ -1,23 +1,40 @@
 package com.w00tmast3r.skquery.elements.expressions;
 
-import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
 import com.w00tmast3r.skquery.SkQuery;
 import com.w00tmast3r.skquery.api.Patterns;
+import com.w00tmast3r.skquery.util.Collect;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
 
 @Patterns("skquery version")
-public class ExprSkQueryVersion extends SimplePropertyExpression<String, String> {
+public class ExprSkQueryVersion extends SimpleExpression<String> {
+
     @Override
-    protected String getPropertyName() {
-        return "plugin version";
+    protected String[] get(Event e) {
+        return Collect.asArray(SkQuery.getInstance().getDescription().getVersion());
     }
 
     @Override
-    public String convert(String s) {
-        return SkQuery.getInstance().getDescription().getVersion();
+    public boolean isSingle() {
+        return true;
     }
 
     @Override
     public Class<? extends String> getReturnType() {
         return String.class;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "plugin version";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        return true;
     }
 }
