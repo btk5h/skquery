a6e495962cfa1fb27f87994e4aa451d9a5353132
Roll Fizzlebeef <bryan.terce@gmail.com>
2014-10-19 15:46:23 -0700

    Optimize RoutineEvent and FunctionEvent

--- src/main/java/com/w00tmast3r/skquery/elements/events/EvtCustom.java
+++ src/main/java/com/w00tmast3r/skquery/elements/events/EvtCustom.java
@@ -4,8 +4,7 @@ import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Checker;
-import com.w00tmast3r.skquery.elements.events.lang.FunctionEvent;
-import com.w00tmast3r.skquery.elements.events.lang.RoutineEvent;
+import com.w00tmast3r.skquery.elements.events.lang.MethodEvent;
 import org.bukkit.event.Event;
 
 @SuppressWarnings("unchecked")
@@ -21,19 +20,12 @@ public class EvtCustom extends SkriptEvent {
 
     @Override
     public boolean check(final Event event) {
-        if(event instanceof RoutineEvent) return s.check(event, new Checker<String>() {
+        return event instanceof MethodEvent && s.check(event, new Checker<String>() {
             @Override
             public boolean check(String s) {
-                return ((RoutineEvent) event).getCause().equalsIgnoreCase(s);
+                return ((MethodEvent) event).getCause().equalsIgnoreCase(s);
             }
         });
-        else if(event instanceof FunctionEvent) return s.check(event, new Checker<String>() {
-            @Override
-            public boolean check(String s) {
-                return ((FunctionEvent) event).getCause().equalsIgnoreCase(s);
-            }
-        });
-        return false;
     }
 
     @Override
